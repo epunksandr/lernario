3a318a18da591d099094885639e1f59288c68008
oleksandr <oleksandr@epunks.de>
2025-04-07 10:34:33 +0200

    homepage

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def showRegister():
+    return render_template('register.html')
+
+@app.route('/login')
+def showLogin():
+    return render_template('login.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
