07a78bff6715336e8b2fa7caba0591767a17ec1a
josef <j.ewert@westfaliaeurope.com>
2025-05-13 13:06:09 +0200

    SQL-Lite DB mit Dummy Daten

--- services/schueler_service.py
+++ services/schueler_service.py
@@ -1,12 +1,26 @@
-from supabase_folder.supabase_client_manager import SupabaseClientManager
+from flask import jsonify
+
+from services.sqllite_db import query_db
 
 
-def loesche_schueler(id: int):
-    supabase = SupabaseClientManager.get_client()
-
-    supabase.table("students").delete().eq("id", id).execute()
+def loesche_schueler(schueler_id: int):
+    query_db("DELETE FROM schueler WHERE schueler_id=?;", (schueler_id, ), commit=True)
 
 def gib_alle_schueler_mit_grundinformationen():
-    supabase = SupabaseClientManager.get_client()
-    response = supabase.rpc("gib_alle_schueler_mit_grundinformationen", {}).execute()
-    return response.data
+    data = query_db(
+        """
+        select
+            schueler.schueler_id,
+            schueler.vorname,
+            schueler.nachname,
+            klassen.klassenname,
+            COUNT(DISTINCT abwesenheiten.abwesenheit_id) AS abwesenheiten,
+            AVG(noten.note) AS notenschnitt
+        from schueler
+            left JOIN klassen ON schueler.klasse_id = klassen.klasse_id
+            left JOIN abwesenheiten ON schueler.schueler_id = abwesenheiten.schueler_id
+            left JOIN noten ON schueler.schueler_id = noten.schueler_id
+        GROUP BY noten.schueler_id
+    """)
+    print(data)
+    return data
