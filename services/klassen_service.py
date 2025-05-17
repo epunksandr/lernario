d88f6a240865b151b84b3fb92d260eed480922bd
oleksandr <oleksandr@epunks.de>
2025-05-17 20:05:39 +0200

    klassenliste fix und klasseninfo schueleranzahl

--- services/klassen_service.py
+++ services/klassen_service.py
@@ -12,11 +12,21 @@ class KlassenService(KlassenBaseService):
     def gib_klassen_mit_schueleranzahl_von_lehrer(self, lehrer_id: int):
         session = SessionLocal()
         klassen = (session.query(Klasse.klasse_id, Klasse.klassenname, func.count(Schueler.schueler_id).label("schueler_anzahl"))
-                   .join(Schueler, Klasse.klasse_id == Schueler.klasse_id)
-                   .filter(Klasse.lehrer_id == lehrer_id)
-                   .all())
+                    .outerjoin(Schueler, Klasse.klasse_id == Schueler.klasse_id)
+                    .filter(Klasse.lehrer_id == lehrer_id)
+                    .group_by(Klasse.klasse_id, Klasse.klassenname)
+                    .all()
+        )
         session.close()
         return klassen
+    
+    def gib_schueleranzahl_von_klasse(self, klasse_id: int):
+        session = SessionLocal()
+        schueler_anzahl = (session.query(func.count(Schueler.schueler_id))
+                           .filter(Schueler.klasse_id == klasse_id)
+                           .scalar())
+        session.close()
+        return schueler_anzahl
 
     def gib_klassen_von_lehrer(self, lehrer_id: int):
         session = SessionLocal()
