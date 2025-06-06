93a9dd2f1e1a4ac3b6af98ad5dea3eee90691649
Marlon Dinter <m.dinter@simplicity.ag>
2025-06-06 17:48:22 +0200

    WIP

--- tests/integration/test_database_function.py
+++ tests/integration/test_database_function.py
@@ -1,7 +1,9 @@
 from tests import fixture
 from sqlalchemy import Column, Integer, String, ForeignKey
+from services.lehrer_service import LehrerService
 
 fixture
+lehrerService = LehrerService()
 
 def test_write_data_in_table():
     assert True
